8e872817731f3e362c5eac8ea3140666cbbb8ef6
Josh Steele <josh.steele@jhuapl.edu>
2024-05-14 13:46:30 -0400

    Addresses #29 updates or adds files for push to github

---
+++ test/edu/jhuapl/sbmt/stateHistory/model/TestStateHistoryUtil.java
@@ -0,0 +1,46 @@
+package edu.jhuapl.sbmt.stateHistory.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class TestStateHistoryUtil
+{
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception
+	{
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception
+	{
+	}
+
+	@Test
+	void testBinarySearch()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBinaryFileLength()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReadString()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReadBinary()
+	{
+		fail("Not yet implemented");
+	}
+
+}
